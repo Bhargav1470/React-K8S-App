ae4a504d38c7d08281154ae0bd3bf5e106d5a912
Bhargav <bhargav.m@sigmoidanalytics.com>
2025-02-02 17:43:03 +0530

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# React-K8S-App
---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>React App Deployed on Kubernetes 🚀</h1>
+      <p>Welcome to your Kubernetes-powered React app!</p>
+    </div>
+  );
+}
+
+export default App;
+
